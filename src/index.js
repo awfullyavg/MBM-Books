31faf3fce8ee53dc0ac9984db0a3509b51d09cd5
Macolister <bispo.mac@gmail.com>
2023-05-15 16:40:05 -0400

    add hello world string to see search result div

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 //Code here
 fetch("http://localhost:3000/books")
 .then(resp => resp.json())
-.then(books => console.log(books))
+.then((books) => searchBooks(books))
+
+function searchBooks(books){
+    const searchBar = document.querySelector("#search-text") //grab the searchbar 
+    //console.log(searchBar)
+
+    const searchResults = document.querySelector("#search-results")
+    console.log(searchResults)
+
+}
+
+
