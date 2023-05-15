ba380c6f585216d8708198e7860020833c83ee38
Macolister <bispo.mac@gmail.com>
2023-05-15 16:40:05 -0400

    add hello world string to see search result div

--- src/index.js
+++ src/index.js
@@ -7,55 +7,16 @@ donationForm.addEventListener("submit", (event) => {
 
 })
 fetch("http://localhost:3000/books")
-    .then(resp => resp.json())
-    .then(data => {
-        data.forEach((book) => addBookToCatalog(book))
-    })
+.then(resp => resp.json())
+.then((books) => searchBooks(books))
 
-function addBookToCatalog(book) {
-    const newBook = document.createElement("span");
-    book.className = "book";
+function searchBooks(books){
+    const searchBar = document.querySelector("#search-text") //grab the searchbar 
+    //console.log(searchBar)
 
-    const title = document.createElement("h3");
-    title.textContent = book.title;
+    const searchResults = document.querySelector("#search-results") //grab the search result div
+    //console.log(searchResults)
 
-    const author = document.createElement("h4");
-    author.textContent = `By: ${book.author}`;
-    
-    const copies = document.createElement("p");
-    copies.textContent = `Copies Available: ${book.copies}`;
-
-    const cover = document.createElement("img");
-    cover.src = book.img_front;
-
-    newBook.appendChild(title);
-    newBook.appendChild(author);
-    newBook.appendChild(copies);
-    newBook.appendChild(cover);
-
-    catalog.appendChild(newBook);
 }
 
-function donatedBook(event) {
-    fetch("http://localhost:3000/books", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json"
-      },
-      body: JSON.stringify({
-        "title": event.target.title.value,
-        "author": event.target.author.value,
-        "pages": event.target.pages.value,
-        "copies": event.target.copies.value,
-        "img_front": event.target.frontImage.value,
-        "img_back": event.taget.backImage.value,
-        "genre": event.target.genre.value,
-        "series": event.target.series.value,
-      })
-    })
-      .then(resp => resp.json())
-      .then(data => {
-        addBookToCatalog(data);
-      })
-  }
+
